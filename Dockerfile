bc707f731eb8ab881bb4e7cd77110cafd33759aa
notariq <hauzanbakri@example.com>
2024-08-18 12:00:57 +0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM node:20.14.0
+
+# Create and set the working directory
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code
+COPY . .
+
+# Expose the port the app runs on
+EXPOSE 8080
+
+# Command to run the application
+CMD ["node", "api-gateway.js"]
---
+++ api-gateway.js
@@ -0,0 +1,138 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// Create an instance of Express app
+const app = express();
+
+
+// Middleware setup
+app.use(cors()); // Enable CORS
+app.use(helmet()); // Add security headers
+app.use(morgan("combined")); // Log HTTP requests
+app.disable("x-powered-by"); // Hide Express server information
+
+// Development
+const services = [
+  {
+  route: "/user",
+  target: process.env.USER_URL || "http://localhost:3001/api/users",
+  },
+  {
+    route: "/music",
+    target: process.env.SONG_URL || "http://localhost:4000/api/music",
+    },
+  {
+    route: "/playlist",
+    target: process.env.PLAYLIST_URL || "http://localhost:5000/api/playlist",
+  },
+  {
+    route: "/auth",
+    target: process.env.AUTH_URL || "http://localhost:3001/api/auth",
+  },
+];
+   
+// Production
+/*
+const services = [
+  {
+    route: "/user",
+    target: "http://sonata-userservice:6000/api/users",
+  },
+  {
+    route: "/music",
+    target: "http://sonata-musicservice:4000/api/music",
+  },
+  {
+    route: "/playlist",
+    target: "http://sonata-playlistservice:5000/api/playlist",
+  },
+];
+*/
+
+// Define rate limit constants
+const rateLimit = 60; // Max requests per minute
+const interval = 60 * 1000; // Time window in milliseconds (1 minute)
+
+// Object to store request counts for each IP address
+const requestCounts = {};
+
+// Reset request count for each IP address every 'interval' milliseconds
+setInterval(() => {
+  Object.keys(requestCounts).forEach((ip) => {
+    requestCounts[ip] = 0; // Reset request count for each IP address
+  });
+}, interval);
+
+// Middleware function for rate limiting and timeout handling
+function rateLimitAndTimeout(req, res, next) {
+  const ip = req.ip; // Get client IP address
+
+  // Update request count for the current IP
+  requestCounts[ip] = (requestCounts[ip] || 0) + 1;
+
+  // Check if request count exceeds the rate limit
+  if (requestCounts[ip] > rateLimit) {
+    // Respond with a 429 Too Many Requests status code
+    return res.status(429).json({
+      code: 429,
+      status: "Error",
+      message: "Rate limit exceeded.",
+      data: null,
+    });
+  }
+
+  // Set timeout for each request (example: 10 seconds)
+  req.setTimeout(15000, () => {
+    // Handle timeout error
+    res.status(504).json({
+      code: 504,
+      status: "Error",
+      message: "Gateway timeout.",
+      data: null,
+    });
+    req.abort(); // Abort the request
+  });
+
+  next(); // Continue to the next middleware
+}
+
+// Apply the rate limit and timeout middleware to the proxy
+app.use(rateLimitAndTimeout);
+
+// Set up proxy middleware for each microservice
+services.forEach(({ route, target }) => {
+  // Proxy options
+  const proxyOptions = {
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      [`^${route}`]: "",
+    },
+  };
+
+  // Apply rate limiting and timeout middleware before proxying
+  app.use(route, rateLimitAndTimeout, createProxyMiddleware(proxyOptions));
+});
+
+
+// Handler for route-not-found
+app.use((_req, res) => {
+    res.status(404).json({
+      code: 404,
+      status: "Error",
+      message: "Route not found.",
+      data: null,
+    });
+   });
+
+// Define port for Express server
+const PORT = process.env.PORT || 8080;
+
+
+// Start Express server
+app.listen(PORT, () => {
+ console.log(`API Gateway is running on port ${PORT}`);
+});
